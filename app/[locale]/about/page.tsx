4d78194920bce7d757f09394f0fd19e870d1c7ff
BhandavyaBv <bh626198@dal.ca>
2026-04-07 18:47:51 -0300

    Removed unwanted files

--- app/[locale]/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutSection from "@/components/sections/AboutSection";
-
-export default function AboutPage() {
-  return (
-    <section className="mx-auto max-w-7xl px-6 py-16">
-      <AboutSection />
-    </section>
-  );
-}
